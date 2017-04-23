8dbbd388ee8748c9e97e7f5bdd6d81c4c675ffa2
Javier López <m@javier.io>
2017-04-23 00:36:27 +0000

    add uber type services
        - references:
            - http://stackoverflow.com/questions/30626030/can-you-force-a-react-component-to-rerender-without-calling-setstate
            - http://stackoverflow.com/questions/37401635/react-js-wait-for-setstate-to-finish-before-triggering-a-function

--- android_ios/index.js
+++ android_ios/index.js
@@ -20,10 +20,11 @@ const searchIcon = require('../assets/img/search.png')
 
 export default class UberFooBarReactNativeFirebase extends Component {
     state = {
-        mapRegion:   null,
+        mapRegion:         null,
         passengerLocation: null,
-        ubers: null,
-        gpsAccuracy: null
+        ubers:             null,
+        type:              'x',
+        gpsAccuracy:       null,
     }
     watchID = null
 
@@ -46,10 +47,10 @@ export default class UberFooBarReactNativeFirebase extends Component {
     componentWillMount() {
         this.watchID = navigator.geolocation.watchPosition((position) => {
             let region = {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                latitudeDelta: 0.00922*1.5,
-                longitudeDelta: 0.00421*1.5
+                latitude:       position.coords.latitude,
+                longitude:      position.coords.longitude,
+                latitudeDelta:  0.00922*1.5,
+                longitudeDelta: 0.00421*1.5,
             }
 
             //let region = this.getRegionFromCoordinates(
@@ -57,14 +58,14 @@ export default class UberFooBarReactNativeFirebase extends Component {
                     //position.coords.longitude,
                     //position.coords.accuracy)
 
-            let coords = {
-                latitude:  region.latitude,
-                longitude: region.longitude
-            }
-
             this.onRegionChange(region, position.coords.accuracy);
 
             if (!this.state.passengerLocation) {
+                let coords = {
+                    latitude:  region.latitude,
+                    longitude: region.longitude,
+                }
+
                 this.onPassengerLocationChange(coords);
             };
         });
@@ -76,8 +77,8 @@ export default class UberFooBarReactNativeFirebase extends Component {
 
     onRegionChange(region, gpsAccuracy) {
         this.setState({
-            mapRegion: region,
-            gpsAccuracy: gpsAccuracy || this.state.gpsAccuracy
+            mapRegion:   region,
+            gpsAccuracy: gpsAccuracy || this.state.gpsAccuracy,
         });
     }
 
@@ -88,92 +89,118 @@ export default class UberFooBarReactNativeFirebase extends Component {
     randomNearbyPosition(coords) {
         return {
             latitude:  coords.latitude  + this.getRandomInt(-100,100)/10000,
-            longitude: coords.longitude + this.getRandomInt(-100,100)/10000
+            longitude: coords.longitude + this.getRandomInt(-100,100)/10000,
         }
     }
 
     generateRandomUbers(coords) {
-        return [
-            { id: 1, name: 'Ana',       position: this.randomNearbyPosition(coords) },
-            { id: 2, name: 'John',      position: this.randomNearbyPosition(coords) },
-            { id: 3, name: 'Emely',     position: this.randomNearbyPosition(coords) },
-            { id: 4, name: 'Mike',      position: this.randomNearbyPosition(coords) },
-            { id: 5, name: 'Christene', position: this.randomNearbyPosition(coords) },
-        ];
+        let ubers = {
+            'pool' : [
+                { id: 1,  type: 'pool',  name: 'Ana',       position: this.randomNearbyPosition(coords) },
+                { id: 2,  type: 'pool',  name: 'John',      position: this.randomNearbyPosition(coords) },
+                { id: 3,  type: 'pool',  name: 'Emely',     position: this.randomNearbyPosition(coords) },
+                { id: 4,  type: 'pool',  name: 'Mike',      position: this.randomNearbyPosition(coords) },
+                { id: 5,  type: 'pool',  name: 'Christene', position: this.randomNearbyPosition(coords) },
+            ],
+            'x' : [
+                { id: 6,  type: 'x',     name: 'Alice',     position: this.randomNearbyPosition(coords) },
+                { id: 7,  type: 'x',     name: 'Bob',       position: this.randomNearbyPosition(coords) },
+                { id: 8,  type: 'x',     name: 'Leidi di',  position: this.randomNearbyPosition(coords) },
+                { id: 9,  type: 'x',     name: 'Brayan',    position: this.randomNearbyPosition(coords) },
+                { id: 10, type: 'x',     name: 'Nicol',     position: this.randomNearbyPosition(coords) },
+            ],
+            'black' : [
+                { id: 11, type: 'black', name: 'Yimi',      position: this.randomNearbyPosition(coords) },
+                { id: 12, type: 'black', name: 'Lou',       position: this.randomNearbyPosition(coords) },
+                { id: 13, type: 'black', name: 'Yann',      position: this.randomNearbyPosition(coords) },
+                { id: 14, type: 'black', name: 'Dominique', position: this.randomNearbyPosition(coords) },
+                { id: 15, type: 'black', name: 'Tim',       position: this.randomNearbyPosition(coords) },
+            ]
+        };
+
+        //console.log('this.state.type', this.state.type)
+        //console.log('ubers[this.state.type]', ubers[this.state.type]);
+        return ubers[this.state.type];
     }
 
     onPassengerLocationChange(coords) {
         this.setState({
-            passengerLocation: coords
-        });
-
-        this.setState({
-            ubers: this.generateRandomUbers(coords)
+            passengerLocation: coords,
+            ubers:             this.generateRandomUbers(coords),
         });
 
         console.log(this.state.passengerLocation);
         console.log(this.state.ubers);
     }
 
-  render() {
-      const { passengerLocation, mapRegion, ubers, gpsAccuracy} = this.state;
-      //console.log('mapRegion: '        , mapRegion);
-      //console.log('passengerLocation: ', passengerLocation);
-      //console.log('gpsAccuracy: '      , gpsAccuracy);
-      //console.log('ubers: '            , ubers);
+    updateTypeCallback() {
+        this.setState({ubers: this.generateRandomUbers(this.state.passengerLocation)});
+    }
 
-      if (mapRegion && passengerLocation && ubers) {
-          return (
-              <View style={styles.container}>
+    render() {
+        //Too much magic!!!!
+        const { mapRegion, passengerLocation, ubers, gpsAccuracy} = this.state;
+        //console.log('mapRegion: '        , mapRegion);
+        //console.log('passengerLocation: ', passengerLocation);
+        //console.log('gpsAccuracy: '      , gpsAccuracy);
+        //console.log('ubers: '            , ubers);
+        console.log('render()');
 
-                <GooglePlacesAutocomplete style={styles.search}
-                                          placeholder='Choose Your Location'
-                                          minLength={3} autoFocus={false}
-                                          fetchDetails={true}
-                                          enablePoweredByContainer={false}
-                                          currentLocation={false}
-                                          renderLeftButton={() => <Image style={styles.searchIcon} source={searchIcon}/>}
-                                          query={{
-                                            key: 'AIzaSyDF_xPY72A9X_dy13ud06Lg6Die6BJ_98M',
-                                            language: 'es',
-                                            types: 'geocode', }} />
+        if (mapRegion && passengerLocation && ubers) {
+            return (
+                <View style={styles.container}>
 
-                <MapView style={styles.map} initialRegion={mapRegion}
-                         onRegionChange={this.onRegionChange.bind(this)}>
+                  <GooglePlacesAutocomplete style={styles.search}
+                                            placeholder='Choose Your Location'
+                                            minLength={3} autoFocus={false}
+                                            fetchDetails={true}
+                                            enablePoweredByContainer={false}
+                                            currentLocation={false}
+                                            renderLeftButton={() => <Image style={styles.searchIcon} source={searchIcon}/>}
+                                            query={{
+                                              key:      'AIzaSyDF_xPY72A9X_dy13ud06Lg6Die6BJ_98M',
+                                              language: 'es',
+                                              types:    'geocode', }} />
 
-                  <MapView.Marker draggable coordinate={passengerLocation}
-                                  onDragEnd = {(e) => {this.onPassengerLocationChange(e.nativeEvent.coordinate)}}>
-                  </MapView.Marker>
+                  <MapView style={styles.map} initialRegion={mapRegion}
+                           onRegionChange={this.onRegionChange.bind(this)}>
 
-                  {ubers.map((uber, index) =>
-                      <MapView.Marker title={uber.name} image={uberIcon}
-                      style={styles.uber} coordinate={uber.position} key={uber.id}/>
-                  )}
+                    <MapView.Marker draggable coordinate={passengerLocation}
+                                    onDragEnd = {(e) => {this.onPassengerLocationChange(e.nativeEvent.coordinate)}}>
+                    </MapView.Marker>
 
-                </MapView>
+                    {ubers.map((uber, index) =>
+                        <MapView.Marker title={uber.name} description={uber.type} image={uberIcon}
+                        style={styles.uber} coordinate={uber.position} key={uber.id}/>
+                    )}
 
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button}>
-                        <Text>UberX</Text>
-                    </TouchableOpacity>
+                  </MapView>
 
-                    <TouchableOpacity style={styles.button}>
-                        <Text>Uber Black</Text>
-                    </TouchableOpacity>
+                  <View style={styles.buttonContainer}>
+                      <TouchableOpacity style={styles.button} onPress={() => {
+                          this.setState({type: 'pool'}, this.updateTypeCallback); }}>
+                          <Text>Uber Pool</Text>
+                      </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.button}>
-                        <Text>Uber VIP</Text>
-                    </TouchableOpacity>
+                      <TouchableOpacity style={styles.button} onPress={() => {
+                          this.setState({type: 'x'}, this.updateTypeCallback); }}>
+                          <Text>Uber X</Text>
+                      </TouchableOpacity>
+
+                      <TouchableOpacity style={styles.button} onPress={() => {
+                          this.setState({type: 'black'}, this.updateTypeCallback); }}>
+                          <Text>Uber Black</Text>
+                      </TouchableOpacity>
+                  </View>
                 </View>
+            );
+        } else {
+            return (
+              <View style={styles.container}>
+                  <Text style={styles.loading}>Cargando ... </Text>
               </View>
-          );
-      } else {
-          return (
-            <View style={styles.container}>
-                <Text style={styles.loading}>Cargando ... </Text>
-            </View>
-         );
-      }
+           );
+        }
   }
 }
 
