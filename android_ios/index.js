7de33e8ae85c30e992b851e3b9ba2eb8fff5613f
Javier López <m@javier.io>
2017-05-08 08:14:52 +0000

    add navigation
    - references:
        - https://github.com/h87kg/NavigatorDemo/blob/master/index.android.js
        - http://bitvbit.blogspot.mx/2016/07/firebase-auth-v3-with-react-native.html

--- android_ios/index.js
+++ android_ios/index.js
@@ -1,10 +1,81 @@
 import React, { Component } from 'react';
+import {
+  Navigator,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+} from 'react-native';
+
+import LoginPage          from './components/LoginPage'
 import PickUpLocationPage from './components/PickUpLocationPage'
 
 export default class UberFooBarReactNativeFirebase extends Component {
+
+    SplashPage(navigator) {
+        const logo = require('../assets/img/logo.png')
+
+        setTimeout(() => {
+            navigator.replace({
+                id: 'LoginPageId',
+            });
+        }, 3000);
+
+        return (
+            <View style={SplashPageStyles.container}>
+                <Image source={logo}/>
+                <Text style={SplashPageStyles.splash}>SplashPage</Text>
+            </View>
+        );
+    }
+
+    renderScene(route, navigator) {
+        switch(route.id) {
+            case 'SplashPageId':
+                return this.SplashPage(navigator);
+
+            case 'LoginPageId':
+                return (
+                    <LoginPage navigator={navigator} />
+                );
+
+            case 'PickUpLocationPageId':
+                return (
+                    <PickUpLocationPage />
+                );
+
+            default:
+                return (
+                    <SplashPage navigator={navigator} />
+                );
+        }
+    }
+
     render() {
         return (
-            <PickUpLocationPage />
+            <Navigator
+                initialRoute={{id: 'SplashPageId', name: 'SplashPage'}}
+                renderScene={this.renderScene.bind(this)}
+                configureScene={(route) => {
+                    if (route.sceneConfig) {
+                        return route.sceneConfig;
+                    }
+                    return Navigator.SceneConfigs.FloatFromRight;
+                }} />
         );
-  }
+    }
 }
+
+const SplashPageStyles = StyleSheet.create({
+    container: {
+        flex:1,
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    splash: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
